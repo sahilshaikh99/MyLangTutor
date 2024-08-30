cfd364225417acd91f4afdbf43525a73fd67fd7f
Sahil Shaikh <sahilshaikh8264@gmail.com>
2024-08-30 01:48:57 +0100

    Added Quiz page words dynamic

--- src/components/Quiz.tsx
+++ src/components/Quiz.tsx
@@ -1,8 +1,10 @@
 import { Button, Container, FormControl, FormControlLabel, FormLabel, Radio, RadioGroup } from "@mui/material";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate, useSearchParams } from "react-router-dom";
 import { ArrowBack } from "@mui/icons-material";
 import Stack from '@mui/material/Stack';
+import { useDispatch, useSelector } from "react-redux";
+import { getResult } from '../redux/slices';
 
 const Quiz = () => {
     const [result, setResult] = useState<string[]>([]);
@@ -11,11 +13,21 @@ const Quiz = () => {
 
     const navigation = useNavigate();
 
+    const { words } = useSelector((state: {root : stateType}) => state.root);
+
+    const dispatch = useDispatch();
+
     const nextHandler = (): void => {
+        setResult((prev) => [...prev, ans]);
         setCount((prev) => prev + 1);
-        setResult((prev) => ({...prev, ans}));
         setAns("");
     }
+
+    useEffect(() =>{
+        if(count+1 > words.length) navigation('./result');
+        console.log(result);
+        dispatch(getResult(result));
+    },[result]);
 
     const param = useSearchParams()[0].get("language") as LangType;
 
@@ -27,7 +39,7 @@ const Quiz = () => {
         <hr />
         <Stack direction="row" spacing={2} sx={{ fontSize: '20px', marginTop: "20px", fontWeight: "bold"}}>
             <p>{count + 1}:</p>
-            <p>Sample</p>
+            <p>{words[count]?.word}</p>
         </Stack>
         <br/>
         <FormControl>
@@ -36,12 +48,16 @@ const Quiz = () => {
                 aria-labelledby="demo-radio-buttons-group-label"
                 defaultValue="female"
                 name="radio-buttons-group"
+                value={ans}
                 onChange={(e) => setAns(e.target.value)}>
-                <FormControlLabel value="Option1" control={<Radio />} label="Option1"/>
+                    { words[count]?.options.map((i, index) => (
+                        <FormControlLabel value={i} control={<Radio />} label={i} key={index}/>
+
+                    ))}
             </RadioGroup>
             </FormControl>
         <br/>
-        <Button onClick={() => {count === 7 ? navigation("/result"): nextHandler()}} variant="contained" color="error" fullWidth>{count === 7 ? "text" : "Next"}</Button>
+        <Button onClick={() => {count+1 === words.length ? navigation("/result"): nextHandler()}} variant="contained" color="error" fullWidth>{count === 7 ? "text" : "Next"}</Button>
     </Container>
 };
 
